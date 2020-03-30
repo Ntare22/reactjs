c966f59d88c54da4e5177a43e77c91f296c44058
Jim Ntare <jim.ntare@gmail.com>
2020-03-30 10:27:51 +0200

    learning how to pass argument in event handlers

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,7 +3,6 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     count: 0,
-    tags: ["tag1", "tag2", "tag3"]
   };
 
   // constructor() {
@@ -11,25 +10,21 @@ class Counter extends Component {
   //   this.handleIncrement = this.handleIncrement.bind(this);
   // }
 
-  handleIncrement = () => {
-    this.setState({ count: this.state.count + 1 })
-  }
+  handleIncrement = product => {
+    console.log(product);
+    this.setState({ count: this.state.count + 1 });
+  };
 
   render() {
     return (
       <React.Fragment>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
-          onClick={this.handleIncrement}
+          onClick={() => this.handleIncrement({ id: 1 })}
           className="btn btn-secondary btn-sm"
         >
           Increment
         </button>
-        <ul>
-          {this.state.tags.map(tag => (
-            <li key={tag}>{tag}</li>
-          ))}
-        </ul>
       </React.Fragment>
     );
   }
