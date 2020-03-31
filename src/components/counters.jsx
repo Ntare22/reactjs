b6830913375fa79dbf9cb359b3f418cd387e26ca
Jim Ntare <jim.ntare@gmail.com>
2020-03-31 11:40:57 +0200

    I got introduced to creating stateless functional components

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -1,26 +1,48 @@
-import React, { Component } from "react";
+// import React, { Component } from "react";
+import React from 'react';
 import Counter from "./counter";
-class Counters extends Component {
-  render() {
-    return (
-      <div>
+// class Counters extends Component {
+//   render() {
+//     return (
+//       <div>
+//         <button
+//           onClick={this.props.onReset}
+//           className="btn btn-primary btn-sm m-2"
+//         >
+//           Reset
+//         </button>
+//         {this.props.counters.map(counter => (
+//           <Counter
+//             key={counter.id}
+//             onDelete={this.props.onDelete}
+//             onIncrement={this.props.onIncrement}
+//             counter={counter}
+//           />
+//         ))}
+//       </div>
+//     );
+//   }
+// }
+
+const Counters = ({onReset, counters, onDelete, onIncrement}) => {
+  return ( 
+    <div>
         <button
-          onClick={this.props.onReset}
+          onClick={onReset}
           className="btn btn-primary btn-sm m-2"
         >
           Reset
         </button>
-        {this.props.counters.map(counter => (
+        {counters.map(counter => (
           <Counter
             key={counter.id}
-            onDelete={this.props.onDelete}
-            onIncrement={this.props.onIncrement}
+            onDelete={onDelete}
+            onIncrement={onIncrement}
             counter={counter}
           />
         ))}
       </div>
-    );
-  }
+   );
 }
 
 export default Counters;
